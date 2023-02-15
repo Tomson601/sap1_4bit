02a1b8a919f7b349e4a6b105cd39ebbf0468c4c4
Tomasz Jarko <tomasz.jarko@gmail.com>
2023-02-15 09:22:01 +0100

    Adding structure

--- src/sap.py
+++ src/sap.py
@@ -1,32 +1,67 @@
 # Computer parts: ALU, Register, Clock,
-
-# ALU class:
+# Data can be only loaded from bus.
 
 class ALU:
     def __init__(self, bits=4):
         self.bits = bits
 
-    def add(self, a, b):
+    def _add(self, a, b):
         if len(a) > self.bits or len(b) > self.bits:
             return print("Error: data overflow")
         else:
-            result = [0, 0, 0, 0]
+            result = [0 for i in range(self.bits)]
             carry = 0
+
             a.reverse()
             b.reverse()
+
             for i in range(self.bits):
-                if carry == 1:
-                    result[i] += 1
-                if a[i] + b[i] == 2:
-                    carry = 1
+                if carry == 0:
+                    if a[i] + b[i] < 2:
+                        result[i] = a[i] + b[i]
+                    else:
+                        carry = 1
                 else:
-                    result[i] = a[i] + b[i]
+                    if a[i] + b[i] + carry == 3:
+                        result[i] = 1
+                        carry = 1
+
+                    elif a[i] + b[i] + carry == 2:
+                        result[i] = 0
+                        carry = 1
+
+                    elif a[i] + b[i] + carry == 1:
+                        result[i] = 1
+                        carry = 0
+
             result.reverse()
             return result, carry
 
-ALU = ALU()
+    def _load():
+        pass
+    
+    def _out():
+        pass
 
-a = [0,1,1,0]
-b = [0,1,1,0]
+class BUS:
+    def __init__(self, bits=4):
+        self.bits = bits
 
-print(ALU.add(a, b))
+    def _out(self, data):
+        pass
+
+    def _load(self, data):
+        pass
+
+class REGISTER:
+    def __init__(self, bits=4):
+        self.bits = bits
+
+    def _out(self, data_out):
+        pass
+
+    def _load(self, data_in):
+        pass
+
+    def _data(self, data):
+        pass
